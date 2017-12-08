ad77300162114c78d366cc8acb6025bc90b59c7a
sensejus666 <mtmariusmt@gmail.com>
2017-12-09 00:24:52 +0200

    iki aukok padaryta, Truputi aptvarkyti pavadinimus aukok skilties.

--- index.php
+++ index.php
@@ -5,7 +5,11 @@
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta name="keywords" content="Linksmosiospedutes, gyvunu prieglauda, šuniukų prieglauda, kačių prieglauda, šuo globojimui, katino globa, gyvuno globa..." />
 
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="js/jquery-1.11.0.min.js"></script>
+	<!--Google Fonts-->
 	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+	<link href='//fonts.googleapis.com/css?family=Rambla:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
 
 	
 	<link rel="stylesheet" type="text/css" href="css/style.css">
@@ -15,7 +19,7 @@
 </head>
 <body>
 	<!--banner starts here-->
-	<div class="banner">
+	<div class="banner" id="frontPage">
 		<div class="container">
 			<div class="banner-main">
 				<div class="banner-white-box">
@@ -43,8 +47,8 @@
 					  <div class="top-nav">
 						<span class="menu"><img src="images/menu.png" alt=""/></span>
 						<ul class="nav1">
-							<li><a href="index.html" class="active">Home</a></li>
-							<li><a class="scroll" href="#about">About</a></li>
+							<li><a href="#frontPage" class="active">Pradžia</a></li>
+							<li><a class="scroll" href="#about">Apie mus</a></li>
 							<li><a class="scroll" href="#services">Services</a></li>
 							<li><a class="scroll" href="#gallery">Gallery</a></li>
 							<li><a class="scroll" href="#contact" >Contact</a></li>
@@ -81,6 +85,51 @@
 			</div>
 		</div>
 	</div>
-	<!--header ends here-->
+	<!-------header ends here-------->
+
+	<!--------- about us ------------>
+		<div class="section" id="about">
+			<div class="container">
+				<h3 class="title">Apie mus</h3>
+				<div class="aboutPLframe">
+					<div class="col-md-12 aboutLP">
+						<img src="images/trueLogo.png"><h2 class="title">VšĮ "Linksmosios pėdutės"</h2>
+						<h5>Tai savanoriška šunelių ir katinėlių prieglauda, kurios tikslas - nežudyti gyvūnų, o globoti tol, kol kiekvienas jų ras jiems tinkamus namus. Pagrindinis mūsų pajamų šaltinis yra 2%GPM ir neabejingų žmonių parama.</h5>
+						<p>Pagal iškvietimus mes nevykstame, neturime tam nei teisės, nei tinkamos įrangos, taigi savo arba rastą gyvūną mums reikia atvežti. Visiems be namų likusiems gyvūnėliams globos suteikti mes negalime! Tačiau tais, kurie patenka į mūsų prieglaudą, pasirūpiname visokeriopai. Pasiimdami gyvūną iš "Linksmųjų pėdučių" jūs galite būti tikri, kad pasiimate fiziškai bei psichiškai sveiką, paskiepyą, dažnu atveju sterilizuotą/kastruotą augintinį, apie kurio charakterio savybes būsite informuoti. Jums bus suteikta informacija apie jo auginimą bei mitybą.<br><br> <span>Perskaitėte apibrėžimą, o dabar susipažinkime iš arčiau. Kiekvieną, atvykusį į prieglaudą, pirmoji pasitinka Rika. Ji mano esanti "Linksmųjų pėdučių" direktorė. Nenorėdami įžeisti jos jausmų, su ja šiuo klausimu nesiginčyjam. Tegul vadovauja :) Plačiau apie ją galite paskaityti skiltyje "Šuniukai". Visgi tikroji direktorė yra Natalija. Ji taip pat prireikus būna slaugė, dresuotoja, psichologė, fotografė, kirpėja ir apskritai, visų mūsų globotinių mama. Jai kasdien padeda energijos nestokojanti darbuotoja, šauni dresuotoja, veterainrė ir savo laisvu laiku ateinantys savanoriai. Jie paruošia kiekvieną gyvūną kelionei namo, suteikia jiems pagrindinį išsilavinimą, kurį vėliau turi tęsti jų šeimininkai.<br><br></span> "Linksmosios pėdutės" ir keli specialiai tam atrinkti bei testuoti šunys iš prieglaudos taip pat teikia ir kaniterapijos (bendravimas su šunimi) paslaugas vaikučiams su negalia bei vaikams ir seneliams iš globos namų. Į šias terapijas kartu su Natalija vyksta "Linksmųjų pėdučių" globotiniai Nordas ir Dūda. Kažkada šie šunys tapę nereikalingi, šiandien savo šiluma, gerumu, nuoširdumu ir bendravimu padeda tiems, kuriems toks bendravimas labiausiai reikalingas. <br><br>Savo globotinių mes nedovanojame. Už kiekvieną namo išleidžiamą globotinį prašome 15 eurų aukos. Nedovanojame ne tik todėl, kad visi jie yra paskiepyti, o suaugėliai dar ir kastruoti/sterilizuoti, bet ir todėl, kad vertiname juos kaip asmenybes. Kiekvienas savitas, su savais pliusais ir savais minusais, ir jie visi yra mūsų šeimos nariai, kurie pas mus atsidūrė ne dėl savo pačių klaidų, o dažniausiai dėl šeimininkų neatsakingumo arba nepalankiai susiklosčiusių aplinkybių. Mes nesiekiame jais atsikratyti, mes siekiame rasti jiems tinkamus namus.</p> 
+						<br>
+						<p class="linksmosios">Mes esame "LINKSMOSIOS pėdutės"! Ir tik LINKSMOSIOS ;)</p>
+						<br>
+						<div class="iframeWrapper">
+						<iframe width="620" height="320" src="https://www.youtube.com/embed/yj4WfsYBjjc"></iframe>
+						</div>
+						<br><br><br>
+						<p class="paremk">Jei norite paremti linksmąsias mūsų pėdutes bei padėti mūsų terapeutams suteikti džiaugsmo sergantiems ir vienišiems vaikučiams, štai mūsų rekvizitai:</p>
+						<br>
+						<div class="infoLine"></div>
+						<p class="email">Linksmosios pėdutės</p>
+						<p class="email">Sąsk. Nr.: LT967300010135165888</p>
+						<p class="email">Paramos gavėjo kodas: 303039292</p>
+						<div class="payPal">
+							<br>
+							<a href="#"><img src="images/PayPal.png" width="100" height="35"></a>
+						</div>
+						<p class="email">info@linksmosiospedutes.lt</p>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	<!-------- //about us -------->
+	<!--------aukok starts here -------->
+	<div class="service-call">
+		<div class="services-call-left">
+			<img src="images/AUKOK_small.jpg">
+		</div>
+		<div class="services-call-right">
+			<a href="https://www.aukok.lt/projektai/Isgelbek-suniuka-nuo-sunkios-ligos" target="_blank">AUKOTI</a>
+		</div>
+	  <div class="clearfix"> </div>
+	</div>
+	<!--service call end here-->
 </body>
 </html>
