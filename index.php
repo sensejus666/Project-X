3fb0de632b794bbef3d34f49c9f4774fb2a8a858
sensejus666 <mtmariusmt@gmail.com>
2017-12-11 12:36:02 +0200

    iki GYVUNAI sutvarkyta

--- index.php
+++ index.php
@@ -7,12 +7,16 @@
 
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 	<script src="js/jquery-1.11.0.min.js"></script>
+	<!-- bootstrap js-->
+	<!-- <script src="js/bootstrap.min.js"></script> -->
 	<!--Google Fonts-->
 	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
 	<link href='//fonts.googleapis.com/css?family=Rambla:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
 
 	
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
 
 
 	<title>Linksmosios Pėdutės:)</title>
@@ -20,17 +24,15 @@
 <body>
 	<!--banner starts here-->
 	<div class="banner" id="frontPage">
-		<div class="container">
-			<div class="banner-main">
-				<div class="banner-white-box">
-					<a href="index.php"><div class="shelter-title">
-						<img src="images/trueLogo.png">
-						<h1>LINKSMOSIOS PĖDUTĖS</h1>
-						<p>Gyvunų prieglaudos namai</p>
-					</div></a>			 
-				</div>
-				<h3>Mes esame "LINKSMOSIOS pėdutės"! Ir tik LINKSMOSIOS ;)</h3>
+		<div class="banner-main">
+			<div class="banner-white-box">
+				<a href="index.php"><div class="shelter-title">
+					<img src="images/trueLogo.png">
+					<h1>LINKSMOSIOS PĖDUTĖS</h1>
+					<p>Gyvunų prieglaudos namai</p>
+				</div></a>			 
 			</div>
+			<h3>Mes esame "LINKSMOSIOS pėdutės"! Ir tik LINKSMOSIOS ;)</h3>
 		</div>
 	</div>
 	<!--banner ends here-->
@@ -38,7 +40,7 @@
 	<!--header starts here-->
 	<div class="header-bottom">
 	  <div class="fixed-header">	
-		<div class="container">
+		<div class="box1">
 			<div class="header-main">
 				<div class="header-left">
 					<h2><a href="index.html">Linksmosios Pėdutės</a></h2>
@@ -49,7 +51,7 @@
 						<ul class="nav1">
 							<li><a href="#frontPage" class="active">Pradžia</a></li>
 							<li><a class="scroll" href="#about">Apie mus</a></li>
-							<li><a class="scroll" href="#services">Services</a></li>
+							<li><a class="scroll" href="#service">Gyvūnai</a></li>
 							<li><a class="scroll" href="#gallery">Gallery</a></li>
 							<li><a class="scroll" href="#contact" >Contact</a></li>
 						</ul>	
@@ -89,11 +91,11 @@
 
 	<!--------- about us ------------>
 		<div class="section" id="about">
-			<div class="container">
+			<div class="box2">
 				<h3 class="title">Apie mus</h3>
-				<div class="aboutPLframe">
-					<div class="col-md-12 aboutLP">
-						<img src="images/trueLogo.png"><h2 class="title">VšĮ "Linksmosios pėdutės"</h2>
+				<div class="col-md-12 aboutLP">
+					<div class="aboutPLframe">
+						<img class="trueLogo" src="images/trueLogo.png"><h2 class="title">VšĮ "Linksmosios pėdutės"</h2>
 						<h5>Tai savanoriška šunelių ir katinėlių prieglauda, kurios tikslas - nežudyti gyvūnų, o globoti tol, kol kiekvienas jų ras jiems tinkamus namus. Pagrindinis mūsų pajamų šaltinis yra 2%GPM ir neabejingų žmonių parama.</h5>
 						<p>Pagal iškvietimus mes nevykstame, neturime tam nei teisės, nei tinkamos įrangos, taigi savo arba rastą gyvūną mums reikia atvežti. Visiems be namų likusiems gyvūnėliams globos suteikti mes negalime! Tačiau tais, kurie patenka į mūsų prieglaudą, pasirūpiname visokeriopai. Pasiimdami gyvūną iš "Linksmųjų pėdučių" jūs galite būti tikri, kad pasiimate fiziškai bei psichiškai sveiką, paskiepyą, dažnu atveju sterilizuotą/kastruotą augintinį, apie kurio charakterio savybes būsite informuoti. Jums bus suteikta informacija apie jo auginimą bei mitybą.<br><br> <span>Perskaitėte apibrėžimą, o dabar susipažinkime iš arčiau. Kiekvieną, atvykusį į prieglaudą, pirmoji pasitinka Rika. Ji mano esanti "Linksmųjų pėdučių" direktorė. Nenorėdami įžeisti jos jausmų, su ja šiuo klausimu nesiginčyjam. Tegul vadovauja :) Plačiau apie ją galite paskaityti skiltyje "Šuniukai". Visgi tikroji direktorė yra Natalija. Ji taip pat prireikus būna slaugė, dresuotoja, psichologė, fotografė, kirpėja ir apskritai, visų mūsų globotinių mama. Jai kasdien padeda energijos nestokojanti darbuotoja, šauni dresuotoja, veterainrė ir savo laisvu laiku ateinantys savanoriai. Jie paruošia kiekvieną gyvūną kelionei namo, suteikia jiems pagrindinį išsilavinimą, kurį vėliau turi tęsti jų šeimininkai.<br><br></span> "Linksmosios pėdutės" ir keli specialiai tam atrinkti bei testuoti šunys iš prieglaudos taip pat teikia ir kaniterapijos (bendravimas su šunimi) paslaugas vaikučiams su negalia bei vaikams ir seneliams iš globos namų. Į šias terapijas kartu su Natalija vyksta "Linksmųjų pėdučių" globotiniai Nordas ir Dūda. Kažkada šie šunys tapę nereikalingi, šiandien savo šiluma, gerumu, nuoširdumu ir bendravimu padeda tiems, kuriems toks bendravimas labiausiai reikalingas. <br><br>Savo globotinių mes nedovanojame. Už kiekvieną namo išleidžiamą globotinį prašome 15 eurų aukos. Nedovanojame ne tik todėl, kad visi jie yra paskiepyti, o suaugėliai dar ir kastruoti/sterilizuoti, bet ir todėl, kad vertiname juos kaip asmenybes. Kiekvienas savitas, su savais pliusais ir savais minusais, ir jie visi yra mūsų šeimos nariai, kurie pas mus atsidūrė ne dėl savo pačių klaidų, o dažniausiai dėl šeimininkų neatsakingumo arba nepalankiai susiklosčiusių aplinkybių. Mes nesiekiame jais atsikratyti, mes siekiame rasti jiems tinkamus namus.</p> 
 						<br>
@@ -120,16 +122,69 @@
 			</div>
 		</div>
 	<!-------- //about us -------->
+
 	<!--------aukok starts here -------->
-	<div class="service-call">
-		<div class="services-call-left">
+	<div class="aukok">
+		<div class="aukok-logo">
 			<img src="images/AUKOK_small.jpg">
 		</div>
-		<div class="services-call-right">
+		<div class="aukok-btn">
 			<a href="https://www.aukok.lt/projektai/Isgelbek-suniuka-nuo-sunkios-ligos" target="_blank">AUKOTI</a>
 		</div>
 	  <div class="clearfix"> </div>
 	</div>
-	<!--service call end here-->
+	<!---------aukok end here---------->
+
+	<!------- iesko namu starts here -------->
+	<div class="services" id="service">
+		<div class="container">
+			<h3 class="title">Gyvūnai</h3>
+			<div class="services-main">
+				<div class="ser-bottom">
+					<div class="col-md-4 ser-grid">
+						 <div class="view view-first">
+		                    <img src="images/suniukas.png" />
+		                    <div class="mask">
+		                        <h2>draugas</h2>	                                              
+		                    </div>
+	                    </div>
+						<h4>Šunys</h4>
+						<p>Šuo - geriausias žmogaus draugas nuo neatmenamų laikų. Jei jo neturi, susirask savo draugą dabar!</p>
+						<div class="ser-btn">
+							<a href="single.html"> Žiūrėti daugiau</a>
+						</div>
+					</div>
+					<div class="col-md-4 ser-grid">
+						<div class="view view-first">
+		                    <img src="images/katukas.png" />
+		                    <div class="mask">
+		                        <h2>namų šiluma</h2>	                                              
+		                    </div>
+	                    </div>
+						<h4>Katės</h4>
+						<p>Katė yra visiškai nuoširdi. Žmonės dėl vienos ar kitos priežasties slepia savo jausmus, bet tik ne katė.</p>
+						<div class="ser-btn">
+							<a href="single.html">Žiūrėti daugiau</a>
+						</div>
+					</div>
+					<div class="col-md-4 ser-grid">
+						<div class="view view-first">
+		                    <img src="images/radeNamus.png" />
+		                    <div class="mask">
+		                        <h2>rade namus</h2>	                                              
+		                    </div>
+	                    </div>
+						<h4>Laiminga Šeima</h4>
+						<p>Gyvūnai gali tapti ne tik tikrais šeimos nariais. Jie duoda naudos ir mūsų fizinei bei emocinei sveikatai.</p>
+						<div class="ser-btn">
+							<a href="single.html">Žiūrėti daugiau</a>
+						</div>
+					</div>
+				 <div class="clearfix"> </div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--------iesko namu end here------------>
 </body>
 </html>
